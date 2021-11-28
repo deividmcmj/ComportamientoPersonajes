4ecf5025a5146fd9adce42efdaf1b58170fd69f1
deividmcmj <48552318+deividmcmj@users.noreply.github.com>
2021-11-28 14:12:29 +0100

    Creación máquina de estados humano normal

--- Comportamiento de Personajes/Assets/Scripts/NormalHumanBehaviour.cs
+++ Comportamiento de Personajes/Assets/Scripts/NormalHumanBehaviour.cs
@@ -4,15 +4,95 @@ using UnityEngine;
 
 public class NormalHumanBehaviour : MonoBehaviour
 {
+    private StateMachineEngine fsm;
+    private State deambulandoState, humanoState, heridoState, infectandoState;
+    private StateMachineEngine fsmHumano;
+    private State huyendoState, escalandoState, armandoState, rindiendoState;
+    private StateMachineEngine fsmHerido;
+    private State suplicandoState, aproximandoState, curandoState;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Máquinas de estados
+        fsm = new StateMachineEngine();
+        fsmHumano = new StateMachineEngine(true);
+        fsmHerido = new StateMachineEngine(true);
+
+        //Estados
+        deambulandoState = fsm.CreateEntryState("Deambulando", Deambular);
+
+        huyendoState = fsmHumano.CreateEntryState("Huyendo", Huir);
+
+        humanoState = fsm.CreateSubStateMachine("Humano", fsmHumano);
+
+        escalandoState = fsmHumano.CreateState("Escalando", Escalar);
+        armandoState = fsmHumano.CreateState("Armando", Armar);
+        rindiendoState = fsmHumano.CreateState("Rindiendo", Rendirse);
+
+        suplicandoState = fsmHerido.CreateEntryState("Suplicando", Suplicar);
+
+        heridoState = fsm.CreateSubStateMachine("Herido", fsmHerido);
+
+        aproximandoState = fsmHerido.CreateState("Aproximando", Aproximar);
+        curandoState = fsmHerido.CreateState("Curando", Curar);
+
+        infectandoState = fsm.CreateState("Infectando", Infectar);
+
+        //Percepciones
+        Perception zombiesCerca = fsm.CreatePerception<ValuePerception>();
+
+        //Transiciones
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        fsm.Update();
+    }
+
+    private void Deambular()
+    {
         
+    }
+
+    private void Huir()
+    {
+
+    }
+
+    private void Escalar()
+    {
+
+    }
+
+    private void Armar()
+    {
+
+    }
+
+    private void Rendirse()
+    {
+
+    }
+
+    private void Suplicar()
+    {
+
+    }
+
+    private void Aproximar()
+    {
+
+    }
+
+    private void Curar()
+    {
+
+    }
+
+    private void Infectar()
+    {
+
     }
 }
